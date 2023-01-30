046952cd6daa3878dbb8d9d0232d7ada96f78c25
maaran <maaranfamily22@gmail.com>
2023-01-30 18:47:43 +0530

    LandingPagepending

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './APP-COMPONENTS/LandingPage/landingpage/landingpage.component';
 
-const routes: Routes = [];
+export const routes: Routes = [ 
+  { path: '', redirectTo: 'Home', pathMatch: 'full', }, 
+  { path: 'home', component: LandingpageComponent, data: { title: 'Login Page' } }, 
+  
+
+]; 
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './APP-COMPONENTS/Dashboard/dashboard/dashboard.component';
+import { LoginComponent } from './APP-COMPONENTS/Login/login/login.component';
+import { LandingpageComponent } from './APP-COMPONENTS/LandingPage/landingpage/landingpage.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
