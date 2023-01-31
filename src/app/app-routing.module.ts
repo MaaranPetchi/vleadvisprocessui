e542b130e774d3dcd836b7f8273839793ec2c68e
maaran <maaranfamily22@gmail.com>
2023-01-31 17:50:11 +0530

    AngularUIofvlea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingpageComponent } from './APP-COMPONENTS/LandingPage/landingpage/landingpage.component';
+import { HomeComponent } from './APP-COMPONENTS/home/home.component';
+
+import { LoginComponent } from './APP-COMPONENTS/Login/login/login.component';
+import { NavbarComponent } from './APP-COMPONENTS/navbar/navbar.component';
+import { SidebarComponent } from './APP-COMPONENTS/sidebar/sidebar.component';
+
 
 export const routes: Routes = [ 
   { path: '', redirectTo: 'Home', pathMatch: 'full', }, 
-  { path: 'home', component: LandingpageComponent, data: { title: 'Login Page' } }, 
-  
-
+  { path: 'navbar', component: NavbarComponent, data: { title: 'Login Page' } }, 
+  { path: 'login', component: LoginComponent, data: { title: 'Login Page' } }, 
+  { path: 'sidenavbar', component: SidebarComponent, data: { title: 'Login Page' } }, 
+  { path: 'home', component: HomeComponent, data: { title: 'Login Page' } }, 
 ]; 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './APP-COMPONENTS/Dashboard/dashboard/dashboard.component';
 import { LoginComponent } from './APP-COMPONENTS/Login/login/login.component';
-import { LandingpageComponent } from './APP-COMPONENTS/LandingPage/landingpage/landingpage.component';
+// import { LandingpageComponent } from './APP-COMPONENTS/LandingPage/landingpage/landingpage.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './APP-COMPONENTS/navbar/navbar.component';
+import { EmployeeComponent } from './APP-COMPONENTS/EmployeeController/Components/employee/employee.component';
+import { HomeComponent } from './APP-COMPONENTS/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { EmployeeModule } from './APP-COMPONENTS/EmployeeController/EmployeeModule/employee/employee.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     LoginComponent,
-    LandingpageComponent
+    // LandingpageComponent,
+    NavbarComponent,
+    EmployeeComponent,
+    HomeComponent,
+    
+    // ReactiveFormsModule
+   
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
